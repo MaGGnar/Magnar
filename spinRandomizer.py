7260c59a995daef9f3cd6356ed93edc2408ccd13
Sondre Lindkjølen <sondredl@gmail.com>
2022-12-28 00:33:35 +0100

    generates correct spint to take

--- spinRandomizer.py
+++ spinRandomizer.py
@@ -4,8 +4,20 @@ import numpy
 print("Hello Magnar")
 
 def randomNumber():
-    numberToPrint = numpy.random.rand()
-    print(numberToPrint)
+    numberToPrint = numpy.random.randint(1,7)
+    # print(numberToPrint)
+    return numberToPrint
 
-randomNumber()
+def spinToTake():
+    spin = randomNumber() * 180
+    return spin
+
+def iterator():
+    i = 0
+    for i in range(20):
+        randomNumber()
+
+# iterator()
+print("spin to take")
+print(spinToTake())
 
